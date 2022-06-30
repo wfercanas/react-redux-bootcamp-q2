f134869a3ef55a46c80c0b582f1700771084cea3
wfercanas <fernando.canas@outlook.com>
2022-06-29 19:25:47 -0500

    change/ yellow add to cart button

--- src/components/Buttons/styles.js
+++ src/components/Buttons/styles.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const StyledAddToCartButton = styled.button`
   padding: 8px 16px;
   border: none;
-  background-color: var(--n900);
-  color: var(--n10);
+  background-color: var(--y200);
+  color: var(--n900);
 `;
 
 export { StyledAddToCartButton };
