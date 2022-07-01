24337b35c6cd6e952515e76163a28fb3c3017747
wfercanas <fernando.canas@outlook.com>
2022-06-30 21:18:53 -0500

    rename/ rename file as interface.jsx

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import products from "../../mocks/products.json";
-import { ProductsUI } from "./Products.interface";
+import { ProductsUI } from "./interface";
 
 const Products = () => {
   return <ProductsUI products={products} />;
