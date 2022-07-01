262dbfa98a2ccc337e27c0fcdc50db164a7dbf1a
wfercanas <fernando.canas@outlook.com>
2022-06-30 22:36:15 -0500

    add/ cart page container reading mock file

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { CartUI } from "./interface";
+import products from "../../mocks/products.json";
+
 const Cart = () => {
-  return;
+  const cart = products.data.products.items.splice(0, 3);
+  let items = 0;
+  let total = 0;
+
+  cart.forEach((product) => {
+    product.quantity = Math.floor(Math.random() * 3 + 1);
+    product.total = product.quantity * product.price;
+    items += product.quantity;
+    total += product.total;
+  });
+
+  return <CartUI cart={cart} items={items} total={total} />;
 };
 
 export { Cart };
